3bc090f5f35963e2aea80a82ce017fa9a567c32f
TALEB Abdellah <t.abdou@live.com>
2021-04-10 11:34:57 +0100

    first commit

---
+++ dom.js
@@ -0,0 +1,52 @@
+function createNewNote() {
+    var title = document.getElementById('title').value
+    var content = document.getElementById('content').value
+
+    var card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    var head = document.createElement('div')
+    head.setAttribute('class', 'head')
+    card.appendChild(head)
+
+    var headspan = document.createElement('span')
+    headspan.innerHTML = title
+    head.appendChild(headspan)
+
+    var headimg = document.createElement('img')
+    headimg.setAttribute('src', 'images/delet.svg')
+    headimg.setAttribute('onclick', 'remove(this)')
+    head.appendChild(headimg)
+
+    var text = document.createElement('div')
+    text.setAttribute('class', 'text')
+    card.appendChild(text)
+
+    var spantext = document.createElement('span')
+    spantext.innerHTML = content
+    text.appendChild(spantext)
+
+    var content = document.getElementById('id-content')
+    content.appendChild(card)
+    dShow();
+}
+
+
+function remove(el) {
+    var toRemove = el.parentNode.parentNode
+    toRemove.remove()
+}
+
+function show() {
+    document.getElementById('form')
+        .style.display = 'flex'
+    document.getElementById('id-content')
+        .style.display = 'none'
+}
+
+function dShow() {
+    document.getElementById('form')
+        .style.display = 'none'
+    document.getElementById('id-content')
+        .style.display = 'flex'
+}
